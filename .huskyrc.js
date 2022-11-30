0652b14883b7cb6941ead351e81dbf80c338e031
daasc <paulojhol@gmail.com>
2022-11-30 10:20:21 -0300

    feat: add git configuration

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   hooks: {
-    "pre-commit": "lint-staged",
+    "pre-commit": "lint-staged && npm run test:unit",
+    "lint-staged": {
+      "*.js": [ "lint" ],
+      "*.vue": [ "lint" ]
+   }   
   },
 };
